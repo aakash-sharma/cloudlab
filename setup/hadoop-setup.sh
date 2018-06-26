326f709f67ff7b6bc59895aa7a5d0367a8eaeeec
Aakash Sharma <asharma.com@gmail.com>
2018-06-25 23:42:56 -0400

    added perf tools

--- setup/hadoop-setup.sh
+++ setup/hadoop-setup.sh
@@ -292,4 +292,6 @@ service auditd restart
 
 sed -i -e 's@^GRUB_CMDLINE_LINUX_DEFAULT=\"\"@GRUB_CMDLINE_LINUX_DEFAULT=\"audit=1\"@' /etc/default/grub
 
+git clone https://github.com/brendangregg/perf-tools.git
+/users/aakashsh/perf-tools/execsnoop > /users/aakashsh/execsnoop.out
 #init 6
